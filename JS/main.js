b3634ccabc9e974491f33823f737efd81cb71497
bthol <95544562+bthol@users.noreply.github.com>
2022-07-28 12:23:10 -0700

    switch color theme with button

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,12 @@
-console.log("JS online");
+const btnSilver = document.querySelector(`#silver-lining-theme`);
+btnSilver.addEventListener("click", setThemeSilver);
+function setThemeSilver() {
+    document.body.classList.remove("color-theme-2");
+    document.body.classList.add("color-theme-1");
+};
+const btnFuturo = document.querySelector(`#futuro-theme`);
+btnFuturo.addEventListener("click", setThemeFuturo);
+function setThemeFuturo() {
+    document.body.classList.remove("color-theme-1");
+    document.body.classList.add("color-theme-2");
+};
