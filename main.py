0372a52d34493692e60dc419e6f20d29701bcc5f
Blake <bthollaug@gmail.com>
2023-07-29 15:20:43 -0700

    remove old operations

--- main.py
+++ main.py
@@ -1,47 +1,5 @@
 import math
 
-def divide(*args):
-    i = 1
-    dividend = args[0]
-    while i < len(args):
-        dividend = dividend / args[i]
-        i = i + 1
-    print(dividend)
-
-# divide(20, 2, 5)
-
-def multiply(*args):
-    i = 1
-    val = args[0]
-    while i < len(args):
-        val = val * args[i]
-        i = i + 1
-    print(val)
-
-# multiply(5, 5, 5)
-
-def add(*args):
-    i = 1
-    sum = args[0]
-    while i < len(args):
-        sum = sum + args[i]
-        i = i + 1
-    print(sum)
-
-# add(1, 2, 1)
-
-def subtract(*args):
-    i = 1
-    difference = args[0]
-    while i < len(args):
-        difference = difference - args[i]
-        i = i + 1
-    print(difference)
-
-# subtract(10, 3, 2)
-
-
-
 def structure_string(str):
     arr = []
     digits = ""
@@ -245,5 +203,6 @@ def calculate(str):
     print(str)
     return solve(structure_string(str))
 
-answer = calculate("2*(5*3^(1+45-44))+10")
+problem = "4+(8*4/2)-(10+2^(2+1)+2)"
+answer = calculate(problem)
 print(answer)
