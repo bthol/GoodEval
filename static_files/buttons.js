71877f2134cf1101ae252761fdb2c7ad49fd8b40
Blake <bthollaug@gmail.com>
2024-10-09 17:52:32 -0700

    zoom proof interface

--- static_files/buttons.js
+++ static_files/buttons.js
@@ -1,5 +1,5 @@
 console.log("linked JavaScript");
-const body = document.querySelector('.body');
+const calc = document.querySelector('.calculator-container');
 const nav = document.querySelector('nav');
 
 // logic for opening and closing the nav menu via the arrow
@@ -10,47 +10,47 @@ navArrow.addEventListener('click', () => {
     if (navState === true) {
         navArrow.classList.remove('downward-arrow');
         navArrow.classList.add('upward-arrow');
-        nav.style.top = '0px';
+        nav.style.top = '0vmin';
     } else {
         navArrow.classList.remove('upward-arrow');
         navArrow.classList.add('downward-arrow');
-        nav.style.top = '-80px';
+        nav.style.top = '-10.5vmin';
     }
 });
 
 // theme buttons for Calculator
 const btnModern = document.querySelector(`#modern-theme`);
 function setThemeModern() {
-    body.classList.remove("color-theme-2");
-    body.classList.remove("color-theme-3");
-    body.classList.remove("color-theme-4");
-    body.classList.add("color-theme-1");
+    calc.classList.remove("color-theme-2");
+    calc.classList.remove("color-theme-3");
+    calc.classList.remove("color-theme-4");
+    calc.classList.add("color-theme-1");
 };
 btnModern.addEventListener("click", setThemeModern);
 
 const btnRetro = document.querySelector(`#retro-theme`);
 function setThemeRetro() {
-    body.classList.remove("color-theme-1");
-    body.classList.remove("color-theme-3");
-    body.classList.remove("color-theme-4");
-    body.classList.add("color-theme-2");
+    calc.classList.remove("color-theme-1");
+    calc.classList.remove("color-theme-3");
+    calc.classList.remove("color-theme-4");
+    calc.classList.add("color-theme-2");
 };
 btnRetro.addEventListener("click", setThemeRetro);
 
 const btnSleek = document.querySelector(`#sleek-theme`);
 function setThemeSleek() {
-    body.classList.remove("color-theme-1");
-    body.classList.remove("color-theme-2");
-    body.classList.remove("color-theme-4");
-    body.classList.add("color-theme-3");
+    calc.classList.remove("color-theme-1");
+    calc.classList.remove("color-theme-2");
+    calc.classList.remove("color-theme-4");
+    calc.classList.add("color-theme-3");
 };
 btnSleek.addEventListener("click", setThemeSleek);
 
 const btnUmbral = document.querySelector(`#umbral-theme`);
 function setThemeUmbral() {
-    body.classList.remove("color-theme-1");
-    body.classList.remove("color-theme-2");
-    body.classList.remove("color-theme-3");
-    body.classList.add("color-theme-4");
+    calc.classList.remove("color-theme-1");
+    calc.classList.remove("color-theme-2");
+    calc.classList.remove("color-theme-3");
+    calc.classList.add("color-theme-4");
 };
 btnUmbral.addEventListener("click", setThemeUmbral);
