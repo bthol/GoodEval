1fda0eb792d235cc85cfe85f731dbc77dc8114ac
Blake <bthollaug@gmail.com>
2024-10-18 15:09:28 -0700

    eval loader

--- static/scripts/evalInterface.js
+++ static/scripts/evalInterface.js
@@ -1,1 +1,33 @@
 console.log('Interface Logic Script Loaded.');
+
+function addDot() {
+    const dot = document.createElement('div');
+    dot.classList.add('elipses-dot');
+    document.querySelector('.elipses-container').appendChild(dot);
+};
+
+function removeDots() {
+    document.querySelector('.elipses-container').innerHTML = '';
+};
+
+let interval = {};
+let intervalCount = 1;
+
+function runAnimation() {
+    addDot();
+    clearInterval(interval);
+    interval = setInterval(() => {
+        if (intervalCount < 8) {
+            addDot();
+            intervalCount += 1;
+        } else {
+            clearInterval(interval);
+            intervalCount = 1;
+            setTimeout(() => {
+                removeDots();
+                runAnimation();
+            }, 600);
+        }
+    }, 525);
+};
+runAnimation();
