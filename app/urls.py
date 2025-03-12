91bfd477952db2258a0740ef517932c96e2a8368
Blake <bthollaug@gmail.com>
2025-03-11 21:25:24 -0700

    landing page

--- app/urls.py
+++ app/urls.py
@@ -4,5 +4,6 @@ from . import views
 # urlConf
 urlpatterns = [
     path("", views.render_index, name="index"),
-    path("eval/", views.evaluate, name="eval"),
+    path("calculator/", views.render_calculator, name="calc"),
+    path("evaluator/", views.render_evaluator, name="eval"),
 ]
--- app/views.py
+++ app/views.py
@@ -4,13 +4,18 @@ from django.http import HttpResponse
 from django.template import loader
 
 from evaluator_file import *
+from django.core.management import call_command
 
 # view functions handle requests and send responses
 
-# The GoodEval Calculator (default)
+# Landing Page
 def render_index(request):
     return render(request, "index.html")
 
+# The GoodEval Calculator (default)
+def render_calculator(request):
+    return render(request, "calculator.html")
+
 # The GoodEval Evaluator
-def evaluate(request):
-    return render(request, "eval.html")
+def render_evaluator(request):
+    return render(request, "evaluator.html")
